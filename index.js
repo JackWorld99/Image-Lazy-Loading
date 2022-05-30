d12d25c3f2c041996484d5e5c07c24a58a270d78
JackWorld <jackwong0326@gmail.com>
2022-05-30 22:19:49 +0800

    Add Intersection Observer scripts.

---
+++ index.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll("img");
+
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const image = entry.target;
+      const data_src = image.getAttribute("data-src");
+      image.setAttribute("src", data_src);
+      observer.unobserve(image);
+      console.log("Trigger");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback);
+
+images.forEach((image) => {
+  observer.observe(image);
+});
